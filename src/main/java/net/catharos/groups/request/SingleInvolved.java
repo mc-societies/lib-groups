e5588cf602ceda70184d0757fe6a89e47dc1c5b2
p000ison <maximilian.ammann@googlemail.com>
2014-06-28 13:55:00 +0200

    Generify requests

---
+++ src/main/java/net/catharos/groups/request/SingleInvolved.java
@@ -0,0 +1,23 @@
+package net.catharos.groups.request;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Represents a SingularInvolved
+ */
+public class SingleInvolved implements Involved {
+    private final Participant participant;
+
+    public SingleInvolved(Participant participant) {this.participant = participant;}
+
+    @Override
+    public boolean isInvolved(Participant participant) {
+        return participant.equals(this.participant);
+    }
+
+    @Override
+    public Collection<? extends Participant> getInvolved() {
+        return Collections.singleton(participant);
+    }
+}
