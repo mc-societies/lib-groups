923e1fd5b22628db3eda38b88e2d107ff2ad4ec8
p000ison <maximilian.ammann@googlemail.com>
2014-09-01 20:02:50 +0200

    Updated translations keys

--- src/main/java/net/catharos/groups/command/GroupParser.java
+++ src/main/java/net/catharos/groups/command/GroupParser.java
@@ -29,14 +29,14 @@ public class GroupParser implements ArgumentParser<Group> {
             Set<Group> groups = provider.getGroup(input).get();
 
             if (groups.isEmpty()) {
-                throw new ParsingException("target.society.not.found", ctx);
+                throw new ParsingException("target-society.not-found", ctx);
             }
 
             return Iterables.getOnlyElement(groups);
         } catch (InterruptedException e) {
-            throw new ParsingException("target.society.not.found", ctx);
+            throw new ParsingException("target-society.not-found", ctx);
         } catch (ExecutionException e) {
-            throw new ParsingException(e, "target.society.not.found", ctx);
+            throw new ParsingException(e, "target-society.not-found", ctx);
         }
     }
 }
