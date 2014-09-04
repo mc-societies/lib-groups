4bf9f00ed39351f6cca16331dac5c118dfdb9c8d
p000ison <maximilian.ammann@googlemail.com>
2014-09-04 16:07:21 +0200

    Added name validators

---
+++ src/main/java/net/catharos/groups/validate/NameValidator.java
@@ -0,0 +1,10 @@
+package net.catharos.groups.validate;
+
+/**
+ * Represents a NameValidator
+ */
+public interface NameValidator {
+
+    ValidateResult validateName(String name);
+
+}
---
+++ src/main/java/net/catharos/groups/validate/TagValidator.java
@@ -0,0 +1,9 @@
+package net.catharos.groups.validate;
+
+/**
+ * Represents a TagValidator
+ */
+public interface TagValidator {
+
+    ValidateResult validateTag(String tag);
+}
