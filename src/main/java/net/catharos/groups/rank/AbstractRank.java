b8cbfcbca2f55428391a4c558b61e600f5987d0f
p000ison <maximilian.ammann@googlemail.com>
2014-11-08 16:17:16 +0100

    cleanup

--- src/main/java/net/catharos/groups/rank/AbstractRank.java
+++ src/main/java/net/catharos/groups/rank/AbstractRank.java
@@ -67,17 +67,7 @@ public abstract class AbstractRank extends AbstractSubject implements Rank {
     @Override
     public boolean hasRule(String rule) {
         Setting<Boolean> setting = rules.get(rule);
-        if (setting == null) {
-            return false;
-        }
-
-        Boolean value = getBoolean(setting);
-
-        if (value == null) {
-            return false;
-        }
-
-        return value;
+        return setting != null && getBoolean(setting);
     }
 
     @Override
