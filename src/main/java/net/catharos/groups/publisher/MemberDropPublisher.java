b1869ef8907a3d69b5faf62eb84f7f6b052c67d4
p000ison <maximilian.ammann@googlemail.com>
2014-11-18 23:19:07 +0100

    Added publishers to drop groups and members

---
+++ src/main/java/net/catharos/groups/publisher/MemberDropPublisher.java
@@ -0,0 +1,12 @@
+package net.catharos.groups.publisher;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import net.catharos.groups.Member;
+
+/**
+ * Represents a MemberDropPublisher
+ */
+public interface MemberDropPublisher {
+
+    <M extends Member> ListenableFuture<M> drop(M member);
+}
